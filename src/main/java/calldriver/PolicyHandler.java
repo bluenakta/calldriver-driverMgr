7f4f524045f0681a235f554377b33792d6c1a86e
logsigma <logsigma@naver.com>
2020-06-18 18:12:12 +0900

    commit

--- src/main/java/calldriver/PolicyHandler.java
+++ src/main/java/calldriver/PolicyHandler.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PolicyHandler{
 
@@ -20,16 +22,26 @@ public class PolicyHandler{
 
         if(driverRequested.isMe()){
 
-            Driver driver = dr.findByStatus("NON").get(0);
+            List<Driver> list = dr.findByStatus("NON");
 
-            if(driver == null) {
+
+
+
+
+            if(list == null || list.size() < 1) {
                 DriverAssignFailed driverAssignFailed = new DriverAssignFailed();
                 driverAssignFailed.setCallId(driverRequested.getId());
                 driverAssignFailed.setStatus("Failed");
+                driverAssignFailed.publish();
+
             } else {
+
+                Driver driver = list.get(0);
                 driver.setStatus("Matched");
+                driver.setCallId(driverRequested.getId());
                 dr.save(driver);
             }
+
     }
 }
     
